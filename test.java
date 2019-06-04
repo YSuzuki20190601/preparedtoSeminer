e69e81e8f0e8d94eb91c984cddcb9a1e6c1f423b
misousuzuki <misou.systex.yuta.suzuki@gmail.com>
2019-06-05 01:38:20 +0900

    test

--- test.java
+++ test.java
@@ -1,3 +1,11 @@
 public static void main(String[] str){
   system.out.println("Hallo world 2");
+	substring(bb);
+}
+
+public String substring(String tempstr){
+	StringBuffer sb = new StringBuffer();
+	sb.append("aa");
+	sb.append(tempstr);
+	return sb.toString();
 }
